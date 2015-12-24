a0d5f7c150b315831360ab9359bb4cb116245d3b
Antti Peltonen <antti.peltonen@iki.fi>
2015-12-24 19:39:45 +0200

    Error handler uses correct mime-type
    CherryPy custom error handler that pushes out JSON also sets correct Content-Type HTTP header.

--- lib/cherry/__init__.py
+++ lib/cherry/__init__.py
@@ -1,4 +1,5 @@
 import json
+import cherrypy
 
 # utility function to dump REST API http errors out in JSON format instead of HTML
 def jsonify_error(status, message, traceback, version):
@@ -7,4 +8,5 @@ def jsonify_error(status, message, traceback, version):
 		'response_type': 'http_error',
 		'http_error': {'status': status, 'message': message}
 	}
+	cherrypy.response.headers["Content-Type"] = "application/json"
 	return json.dumps(error)
