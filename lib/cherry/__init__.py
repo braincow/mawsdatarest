76d123002367b68ee17c39627841102e1c6aafe3
Antti Peltonen <antti.peltonen@iki.fi>
2015-12-24 19:56:07 +0200

    API customer error handler improvement Error handler now adds more info helpful in debugging issues on client side.

--- lib/cherry/__init__.py
+++ lib/cherry/__init__.py
@@ -1,9 +1,13 @@
 import json
 import cherrypy
+import os
 
 # utility function to dump REST API http errors out in JSON format instead of HTML
 def jsonify_error(status, message, traceback, version):
 	error = {
+		'application_id': os.environ.get("OPENSHIFT_APP_NAME"),
+		'api_method': cherrypy.request.method,
+		'api_endpoint': cherrypy.request.path_info,
 		'api_version': 1,
 		'response_type': 'http_error',
 		'http_error': {'status': status, 'message': message}
