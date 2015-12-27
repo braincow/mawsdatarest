cecd66b573aad0137e73378af0c9c75538382ce1
Antti Peltonen <antti.peltonen@iki.fi>
2015-12-27 18:46:50 +0200

    Configuration filenames now more descriptive

--- wsgi.py
+++ wsgi.py
@@ -35,19 +35,19 @@ configdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config')
 if __name__ != "__main__":
 	import atexit
 	# start cherrypy engine and stuff
-	cherrypy.config.update(os.path.join(configdir, 'cherrypy_openshift.conf'))
+	cherrypy.config.update(os.path.join(configdir, 'cherrypy_wsgi.conf'))
 	connect(cherrypy.config.get("mongodb.def"), host=cherrypy.config.get("mongodb.uri"))
 	cherrypy.server.unsubscribe()
 	cherrypy.engine.start()
 	atexit.register(cherrypy.engine.stop)
-	appconfig = os.path.join(configdir, "application_openshift.conf")
+	appconfig = os.path.join(configdir, "application_wsgi.conf")
 	# create and start WSGI application framework / application by CherryPy
 	application = cherrypy.Application(approot(), script_name=None, config=appconfig)
 else:
 	# start cherrypy locally
-	cherrypy.config.update(os.path.join(configdir, 'cherrypy_localhost.conf'))
+	cherrypy.config.update(os.path.join(configdir, 'cherrypy_standalone.conf'))
 	connect(cherrypy.config.get("mongodb.def"), host=cherrypy.config.get("mongodb.uri"))
-	appconfig = os.path.join(configdir, "application_localhost.conf")
+	appconfig = os.path.join(configdir, "application_standalone.conf")
 	cherrypy.quickstart(approot(), '/', config=appconfig)
 
 #eof
